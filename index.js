e8bbcb18e19ae666cb4e71c948f602b9f1a698ae
RamisaBinteAzad <tasnimlamia52@gmail.com>
2022-10-28 19:00:39 +0600

    Final Single Course Data is Added

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
-const courses= require("./data/courses.json");
+const courses = require("./data/courses.json");
+const singleCourse = require("./data/SingleCourse.json");
 app.get("/", (req, res) => {
   res.send("COURSES API Running");
 });
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+app.get("/singleCourse", (req, res) => {
+  res.send(singleCourse);
+});
+ 
+
+app.get("/singleCourse/:id", (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id;
+  const selectedCourse = singleCourse.find((n) => n.id == id);
+  res.send(selectedCourse);
 });
 app.listen(port, () => {
   console.log(" CourCity Server running on port", port);
